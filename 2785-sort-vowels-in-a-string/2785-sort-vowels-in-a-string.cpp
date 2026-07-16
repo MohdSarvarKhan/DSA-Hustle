e11f02e960435a148fbc2bc6e184d9793e3916c6
MohdSarvarKhan <160846862+MohdSarvarKhan@users.noreply.github.com>
2026-07-16 23:57:28 +0530

    Time: 11 ms (79.08%), Space: 15.5 MB (96.14%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -8,32 +8,51 @@ public:
 
     string sortVowels(string s) {
         int n = s.size();
-        string t = s;
-        // vector <string> t;
-        int count[256] = {0};
-        // vector<int> count(10,0);
+//         string t = s;
+//         // vector <string> t;
+//         int count[256] = {0};
+//         // vector<int> count(10,0);
         
-        for(int i = 0; i < n; i++){
-            // count[s[i]]++;
-            if(isVowel(s[i]))
-             count[s[i]]++;
-        }
+//         for(int i = 0; i < n; i++){
+//             // count[s[i]]++;
+//             if(isVowel(s[i]))
+//              count[s[i]]++;
+//         }
 
+//         for(int i = 0; i < n; i++){
+//             if(isVowel(s[i])){
+//                 for (int j = 0; j < 256; j++) {
+//     if (count[j] > 0 && isVowel((char)j)) {
+//         t[i] = (char)j;
+//         count[j]--;
+//         break;
+//     }
+// }
+//             }
+//             else{
+//                 t[i] = s[i];
+//             }
+//         }
+//         return t;
+        string vowels = "";
         for(int i = 0; i < n; i++){
             if(isVowel(s[i])){
-                for (int j = 0; j < 256; j++) {
-    if (count[j] > 0 && isVowel((char)j)) {
-        t[i] = (char)j;
-        count[j]--;
-        break;
-    }
-}
-            }
-            else{
-                t[i] = s[i];
+                vowels += s[i];
             }
         }
-        return t;
+
+        sort(vowels.begin(), vowels.end());
+
+        int j = 0; int i =0;
+        while(i < n){
+            if(isVowel(s[i])){
+                s[i] = vowels[j];
+                j++;
+                
+            }
+            i++;
+        }
+        return s;
 
     }
 };
