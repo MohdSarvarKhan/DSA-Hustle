932cf02d21fdb8d23ed702f0754eef1fa92c9272
MohdSarvarKhan <160846862+MohdSarvarKhan@users.noreply.github.com>
2026-07-31 20:29:16 +0530

    Time: 3 ms (98.09%), Space: 11.4 MB (55.56%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -1,3 +1,49 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//           if(!head||!head->next){
+// 		return NULL;
+// 	}
+        
+//         ListNode * slow = head, *fast = head;
+        
+//         while(fast && fast->next){
+//             // prev = slow;
+//             slow = slow->next;
+//             fast = fast->next->next;
+//             if(slow == fast)
+//             break;
+//         }
+        
+//       if(!fast || !fast->next){
+// 		return NULL;
+// 	}
+        
+//         if(fast == head){
+//         // prev->next =NULL;
+//         return head;
+//         }
+        
+//         slow = head;
+        
+//         while(slow->next != fast->next){
+//             slow = slow->next;
+//             fast = fast->next;
+//         }
+        
+//         // fast->next = NULL;
+//         return slow->next;
+//     }
+// };
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,37 +55,29 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-          if(!head||!head->next){
-		return NULL;
-	}
-        
-        ListNode * slow = head, *fast = head;
-        
-        while(fast && fast->next){
-            // prev = slow;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        bool isCyclic = false;
+
+        while(fast != NULL && fast->next != NULL) {
             slow = slow->next;
             fast = fast->next->next;
-            if(slow == fast)
-            break;
+
+            if(slow == fast) {
+                slow = head;
+
+                while(slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+
+                return slow;
+            }
         }
-        
-      if(!fast || !fast->next){
-		return NULL;
-	}
-        
-        if(fast == head){
-        // prev->next =NULL;
-        return head;
-        }
-        
-        slow = head;
-        
-        while(slow->next != fast->next){
-            slow = slow->next;
-            fast = fast->next;
-        }
-        
-        // fast->next = NULL;
-        return slow->next;
+
+        return NULL;
+
+   
+
     }
 };
