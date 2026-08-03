7ab7363a855bf7b6eb51c144e48471a6002b2812
MohdSarvarKhan <160846862+MohdSarvarKhan@users.noreply.github.com>
2026-08-03 18:13:26 +0530

    Time: 0 ms (100%), Space: 8.4 MB (82.63%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int left = 0, right = 0;
+
+	for(int i=0;i<s.size();i++){
+		if(s[i]=='('){
+			left++;
+		}
+		else{
+			if(left){
+				left--;
+			}
+			else{
+				right++;
+			}
+		}
+	}
+
+	return left+right;
+    }
+};
