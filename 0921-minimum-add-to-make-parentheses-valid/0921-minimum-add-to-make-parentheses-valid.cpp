ee2a6e6c5e0afac24e706f0ef93dcb8f564028ee
MohdSarvarKhan <160846862+MohdSarvarKhan@users.noreply.github.com>
2026-08-03 19:43:44 +0530

    Time: 0 ms (100%), Space: 8.7 MB (16.65%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int left = 0, right = 0;
+    //     int left = 0, right = 0;
 
-	for(int i=0;i<s.size();i++){
-		if(s[i]=='('){
-			left++;
-		}
-		else{
-			if(left){
-				left--;
-			}
-			else{
-				right++;
-			}
-		}
-	}
+	// for(int i=0;i<s.size();i++){
+	// 	if(s[i]=='('){
+	// 		left++;
+	// 	}
+	// 	else{
+	// 		if(left){
+	// 			left--;
+	// 		}
+	// 		else{
+	// 			right++;
+	// 		}
+	// 	}
+	// }
 
-	return left+right;
+	// return left+right;
+
+    stack<char> st;
+
+    for(int i = 0; i < s.size(); i++){
+
+       
+            if(s[i] == ')' && !st.empty() && st.top() == '('){
+                st.pop();
+
+            }
+            else{
+                st.push(s[i]);
+                
+            }
     }
+        return st.size();
+
+    }
+
+    
 };
