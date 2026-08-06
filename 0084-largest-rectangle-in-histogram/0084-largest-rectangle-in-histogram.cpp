911b2c99e708127e030906be17006e10d95c96c6
MohdSarvarKhan <160846862+MohdSarvarKhan@users.noreply.github.com>
2026-08-06 20:32:03 +0530

    Time: 20 ms (64.52%), Space: 81.4 MB (71.57%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> NSR(n);
-        vector<int> NSL(n,-1);
-        
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
         stack<int> st;
+        int max_area = 0;
         
-        for(int i = 0; i < n; i++){
-            while(!st.empty() && arr[st.top()] > arr[i]){
-                NSR[st.top()] = i;
+        // Loop 'n' tak chalega (<= n), taaki aakhir mein bache hue sab pop ho jayein
+        for(int i = 0; i <= n; i++){
+            
+            // i == n par hum ek imaginary 0 height ka bar maante hain
+            // taaki stack mein jo bhi bacha hai wo sab pop ho jaye
+            int curr_height = (i == n) ? 0 : heights[i];
+            
+            while(!st.empty() && curr_height < heights[st.top()]){
+                // Jis element ko nikal rahe hain, uski height lo
+                int h = heights[st.top()];
                 st.pop();
+                
+                // Pop hone ke baad stack ka naya top hi uska NSL hai
+                int nsl = st.empty() ? -1 : st.top();
+                
+                // Current index 'i' uska NSR hai
+                int nsr = i;
+                
+                int width = nsr - nsl - 1;
+                max_area = max(max_area, h * width);
             }
-            st.push(i);
-        }
-        
-        while(!st.empty()){
-        NSR[st.top()] = n;
-        st.pop();
-        }
-        
-        for(int i = n - 1; i >= 0; i--){
-
-            while(!st.empty() && arr[i] < arr[st.top()]){
-                NSL[st.top()] = i;
-                st.pop();
-            }
-
             
             st.push(i);
         }
         
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            ans = max(ans,(NSR[i] - NSL[i] - 1) *arr[i]);
-        }
-        
-        return ans;
+        return max_area;
     }
 };
