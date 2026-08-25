69b32d2b1fd3621b8790c06aa2b5242844992711
MohdSarvarKhan <160846862+MohdSarvarKhan@users.noreply.github.com>
2026-08-25 16:02:32 +0530

    Time: 2 ms (48.91%), Space: 13.4 MB (17.12%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -5,24 +5,19 @@ public:
         priority_queue<int>q(gifts.begin(),gifts.end());
 
         long long ans = 0;
-        for(int i = 0; i < gifts.size();i++){
-            ans+=gifts[i];
-        }
 
-        int remaining;
-        while(k ){
-            remaining = q.top();
-            // int x = q.top();
+        while(k > 0){
+            int x = q.top();
             q.pop();
-            ans -= remaining - sqrt(remaining);
-            q.push(sqrt(remaining));
+            x = sqrt(x);
+            q.push(x);
         k--;
         }
 
-        // while(!q.empty()){
-        //     ans += q.top();
-        //     q.pop();
-        // }
+        while(!q.empty()){
+            ans += q.top();
+            q.pop();
+        }
     return ans;
     }
 };
